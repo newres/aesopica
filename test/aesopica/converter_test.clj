6c2d3362151728e7eabac24ad286c2dc39bb1780
Newres Al Haider <newrescode@gmail.com>
2018-11-10 00:06:36 +0100

    Removed commented out code.

--- test/aesopica/converter_test.clj
+++ test/aesopica/converter_test.clj
@@ -84,44 +84,6 @@
      [:stork :is-cunning true]
      [:dinner1 :has-date {:value "2002-05-30T09:00:00" :type :xsd/dateTime}]}})
 
-;; (deftest valid-empty-context-test
-;;   (let [context {}]
-;;     (is (s/valid? ::aes/context context))))
-
-;; (deftest valid-context-fox-stork-test
-;;   (let [context (::aes/context fox-and-stork-edn)]
-;;     (is (s/valid? ::aes/context context))))
-
-;; (deftest valid-facts-fox-stork-test
-;;   (let [facts (::aes/facts fox-and-stork-edn)]
-;;     (is (s/valid? ::aes/facts facts))))
-
-;; (deftest valid-kb-fox-stork-test
-;;   (let [kg fox-and-stork-edn]
-;;     (is (s/valid? ::aes/knowledge-graph kg))))
-
-;; (deftest valid-context-fox-stork-literals-test
-;;   (let [context (::aes/context fox-and-stork-literals-edn)]
-;;     (is (s/valid? ::aes/context context))))
-
-;; (deftest valid-kb-fox-stork-literals-test
-;;   (let [facts (::aes/facts fox-and-stork-literals-edn)]
-;;     (is (s/valid? ::aes/facts facts))))
-
-;; (deftest valid-kb-fox-stork-test
-;;   (let [kg fox-and-stork-literals-edn]
-;;     (is (s/valid? ::aes/knowledge-graph kg))))
-
-;; (deftest contextualize-base-test
-;;   (let [context {nil "http://www.newresalhaider.com/ontologies/aesop/foxstork/"}
-;;         kw :fox]
-;;     (is (= "http://www.newresalhaider.com/ontologies/aesop/foxstork/fox" (contextualize context kw)))))
-
-;; (deftest contextualize-keyword-test
-;;   (let [context {:foxstork "http://www.newresalhaider.com/ontologies/aesop/foxstork/"}
-;;         kw :foxstork/fox]
-;;     (is (= "http://www.newresalhaider.com/ontologies/aesop/foxstork/fox" (contextualize context kw)))))
-
 (deftest convert-to-resource-test
   (let [context {:foxstork "http://www.newresalhaider.com/ontologies/aesop/foxstork/"}
         kw :foxstork/fox]
